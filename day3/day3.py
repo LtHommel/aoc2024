8a5a3c943e913d76f1cd8a9e1ba72ea00d947617
Wilhelmina <bzzz@hommel.lt>
2024-12-03 15:44:57 +0000

    dag 3 opgepoetst

--- day3/day3.py
+++ day3/day3.py
@@ -9,22 +9,14 @@ regex = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')
 for x,y in regex.findall(data):
     part1 += int(x) * int(y)
 
-# do = [0] + [match.end() for match in re.compile(r"(do)(?!n't)").finditer(data)] # Laat deze toch even staan vanwege de wijze les met het uitsluiten van de substring "n't". Het was alleen niet nodig omdat ik had moeten zoeken op do() en don't(), met haakjes, en ik heb dus een probleem opgelost dat ik helemaal niet had!
-do = [0] + [match.end() for match in re.compile(r"do\(\)").finditer(data)]
-dont = [match.start() for match in re.compile(r"don't\(\)").finditer(data)]
-
-data_part2 = ''
 enabled = True
-for i in range(len(data)):
-    if enabled:
-      data_part2 += data[i]
-    if i in do:
+for do, dont, x, y in re.findall(r'(do\(\))|(don\'t\(\))|mul\((\d{1,3}),(\d{1,3})\)', data):
+    if do:
         enabled = True
-    elif i in dont:
+    elif dont:
         enabled = False
-
-for x,y in regex.findall(data_part2):
-    part2 += int(x) * int(y)
+    elif enabled:
+        part2 += int(x) * int(y)
 
 print('part1: ', part1)
 print('part2: ', part2)
