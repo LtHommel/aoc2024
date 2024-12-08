c05601e1af4401422990c9506960c11a33f27a68
Wilhelmina <bzzz@hommel.lt>
2024-12-08 22:21:14 +0000

    day 4, part 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,16 +1,19 @@
-data = open('example4.txt').readlines()
-# data = open('input4.txt').readlines()
+from util.grid import get_surroundings
+
+# data = open('example4.txt').readlines()
+data = open('input4.txt').readlines()
 
 part1 = part2 = 0
 
-data = [line.strip('\n') for line in data] # verwijder newlines
+data = [line.strip('\n') for line in data]  # verwijder newlines
 
 # diagonalen zoeken keihard gejat van https://stackoverflow.com/a/43311126/7920259
 num_cols = len(data[0])
 num_rows = len(data)
 cols = ['' for _ in range(num_cols)]
 rows = ['' for _ in range(num_rows)]
-fw_diagonals = ['' for _ in range(num_rows + num_cols - 1)] # je kunt een _ gebruiken in plaats van een variabelenaam als je er niet meer naar refereert
+fw_diagonals = ['' for _ in range(
+    num_rows + num_cols - 1)]  # je kunt een _ gebruiken in plaats van een variabelenaam als je er niet meer aan refereert
 bw_diagonals = ['' for _ in range(len(fw_diagonals))]
 offset_bw_diagonals = -num_rows + 1
 
@@ -28,7 +31,15 @@ for strings in cols, rows, fw_diagonals, bw_diagonals:
 
 # ------------ part 2 -----------------
 
-# TODO
+for l, line in enumerate(data):
+    for c, char in enumerate(line):
+        if char == 'A':
+            surroundings = get_surroundings(c, l, data)
+            if ((surroundings[0] == 'M' and surroundings[7] == 'S') or (
+                    surroundings[7] == 'M' and surroundings[0] == 'S')) and (
+                    (surroundings[2] == 'M' and surroundings[5] == 'S') or (
+                    surroundings[5] == 'M' and surroundings[2] == 'S')):
+                part2 += 1
 
 print('part1: ', part1)
 print('part2: ', part2)
