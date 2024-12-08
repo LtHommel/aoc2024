f74a3af786aee4618db6b49777834e6b21290aeb
Wilhelmina <bzzz@hommel.lt>
2024-12-08 12:00:06 +0000

    add functions for adding and substracting tuples

--- util/tuple_arithmetics.py
+++ util/tuple_arithmetics.py
@@ -1,12 +1,14 @@
 def tup_add(a, b):
     """
-
-    :param a: tuple met twee posities
-    :param b:
-    :return:
+    Performs '+' operation on two tuples of length two. For example:
+    tup_add((4,2), (3,1)) returns (7,3)
     """
     return a[0] + b[0], a[1] + b[1]
 
 
 def tup_sub(a, b):
+    """
+        Performs '-' operation on two tuples of length two. For example:
+        tup_add((4,2), (3,1)) returns (1,1)
+        """
     return a[0] - b[0], a[1] - b[1]
